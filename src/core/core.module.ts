45e2db271932712b3be0ac6afdb1a077c9043c60
KyaamiRei <kolyavasilenko2703@gmail.com>
2025-08-06 22:09:36 +0300

    init account

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql/dist/graphql.module';
 
+import { AccountModule } from '../modules/auth/account/account.module';
 import { IS_DEV_ENV } from '../shared/utils/is-dev.util';
 
 import { getGraphQLConfig } from './config/graphql.config';
@@ -23,6 +24,7 @@ import { RedisModule } from './redis/redis.module';
 		}),
 		PrismaModule,
 		RedisModule,
+		AccountModule,
 	],
 })
 export class CoreModule {}
